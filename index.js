8d33051ec565ed991bf2a564c63c66d374c4c24c
Daniel carmona Zapata <77087269+danielcarz@users.noreply.github.com>
2025-09-13 16:21:31 -0500

    configuracion de servidor basico de express y creacion de primera ruta basica

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 //create express app
 const app = express();
 
+//routes
+app.get('/', (req, res)=>{
+
+    res.send('Hello World');
+
+    req.json({ok: true});
+
+    
+});
+
 
 //listen for requests
 app.listen(4000, ()=>{
